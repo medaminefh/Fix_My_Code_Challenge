eaf0b4ed08bd99c93a994310c869dd67b0942b7a
Mohamed Amine Fhal <fhal.mohamed.amine@gmail.com>
2024-02-10 18:51:11 +0100

    fix square area function

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
+""" Square class """
+
 
 class square():
-    
+    """ Square class """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Constructor function"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of the square """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    """ Main function """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
