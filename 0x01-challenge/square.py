adb49f76e4b1bfe7e2aab6f7686cb8b2f7196038
Mohamed Amine Fh <37845480+medaminefh@users.noreply.github.com>
2024-02-13 06:00:18 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-""" Square class """
+""" Fix Square class module """
 
 
 class square():
-    """ Square class """
+    """ Square class declaration """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Constructor function"""
+        """ Constructor function
+        args
+        kwargs
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """ Calculate Area of the square """
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """ Permiter of the square """
+        """ Calculate Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -26,7 +29,7 @@ class square():
 
 
 if __name__ == "__main__":
-    """ Main function """
+    """ Main function Call, if its running in terminal"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
